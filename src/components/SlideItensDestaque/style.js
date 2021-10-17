cbea2f2b266ad157c7ed9944dea51299bf126f0e
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-17 20:56:43 -0300

    Create style.js

---
+++ src/components/SlideItensDestaque/style.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.TouchableOpacity`
+    padding-top: 16px;
+    padding-bottom: 16px;
+    padding-right: 16px;
+    width: 140px;
+    height: 180px;
+    
+
+`;
+
+export const BannerItem = styled.Image`
+    width: 100%;
+    height: 170px;
+    border-radius: 8px;
+    background-color: white;
+`;
+
+export const Titulo = styled.Text`
+    color: #FFF;
+    font-size: 14px;
+    padding-top: 8px;
+`;
