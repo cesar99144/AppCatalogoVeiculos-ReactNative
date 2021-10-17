5286d6c84786237cd5ed681d6bcc0bfcc1679ad6
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-17 17:20:00 -0300

    Create style.js

---
+++ src/components/Header/style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+    height: 70px;
+    flex-direction: row;
+    align-items: center;
+    padding-left: 14px;
+    justify-content: space-between;
+`;
+
+export const MenuBotao = styled.TouchableOpacity`
+    height: 70px;
+    align-items: center;
+    flex-direction: row;
+`;
+
+export const Titulo = styled.Text`
+    color: white;
+    font-size: 25px;
+    font-weight: bold;
+    
+`;
+
+export const FiltroBotao = styled.TouchableOpacity`
+    height: 70px;
+    align-items: center;
+    flex-direction: row;
+    margin-right: 14px;
+`;
