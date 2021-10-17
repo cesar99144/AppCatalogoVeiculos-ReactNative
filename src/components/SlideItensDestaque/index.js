3162ffda815b730652ef5afb0bb219e9ef6bdb90
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-17 20:56:40 -0300

    Renderiza os dados de destaque

---
+++ src/components/SlideItensDestaque/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Container, BannerItem, Titulo } from './style';
+
+export default function SlideItensDestaque({data}) {
+ return (
+    <Container activeOpacity={0.7}>
+        <BannerItem
+            source={{uri: `http://192.168.1.6:4000/imagensVeiculos/${data.primeiraImagem}`}}
+        />
+        <Titulo numberOfLines={1}>{data.tituloAnuncio}</Titulo>
+    </Container>
+  );
+}
