6051b93520e2c370a38de7dfc68ca4aa2ca61a07
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-17 20:56:16 -0300

    Dados vindo da api

--- src/components/SlideItem/index.js
+++ src/components/SlideItem/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { View, Text } from 'react-native';
 import { Container, BannerItem, Titulo } from './style';
 
-export default function SlideItem() {
+export default function SlideItem({data}) {
  return (
     <Container activeOpacity={0.7}>
         <BannerItem
-            source={{uri: 'https://img.elo7.com.br/product/zoom/1043314/kit-adesivo-lateral-gol-g5-g-2p-gs1-faixa-lateral-para-gol.jpg'}}
+            source={{uri: `http://192.168.1.6:4000/imagensVeiculos/${data.primeiraImagem}`}}
         />
-        <Titulo numberOfLines={1}>Gol g5fdfdfdddddddddddd</Titulo>
+        <Titulo numberOfLines={1}>{data.tituloAnuncio}</Titulo>
     </Container>
   );
 }
