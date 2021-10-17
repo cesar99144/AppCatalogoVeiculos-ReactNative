36a18520a0aa33876493a23a7b9de098d8aeb4f9
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-17 17:19:58 -0300

    Componente Header da página home

---
+++ src/components/Header/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import { Feather} from '@expo/vector-icons';
+
+import { Container, FiltroBotao, MenuBotao, Titulo } from "./style";
+import { useNavigation } from "@react-navigation/core";
+
+export default function Header( { titulo }){
+
+    const navigation = useNavigation();
+
+    return(
+        <Container>
+            <MenuBotao onPress={ () => navigation.openDrawer()}>
+                <Feather name="menu" size={36} color="#FFF" />
+            </MenuBotao>
+
+            <Titulo>{titulo}</Titulo>
+
+            <FiltroBotao>
+                <Feather name="filter" size={36} color="#FFF" />
+            </FiltroBotao>
+
+        </Container>
+    );
+}
