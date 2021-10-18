0c175ecf59a82d9944593683565623f4616453e5
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-18 19:21:37 -0300

    Update stackRoutes.js

--- src/routes/stackRoutes.js
+++ src/routes/stackRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import Home from '../pages/Home';
+import Detalhes from "../pages/Detalhes";
 
 const Stack = createNativeStackNavigator();
 
@@ -14,6 +15,15 @@ export default function StackRoutes(){
                 options={{
                     headerShown: false
                 }}
+            />
+            <Stack.Screen
+                name="Detalhes"
+                component={Detalhes}
+                options={{
+                    headerShown: false,
+                    title: 'Detalhes'
+                }}
+                
             />
         </Stack.Navigator>
     );
