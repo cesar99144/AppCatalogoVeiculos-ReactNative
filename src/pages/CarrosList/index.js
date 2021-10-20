987c0e3e11b615b2436f829c15af3eb0a0c6f4de
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-19 21:20:31 -0300

    Create index.js

---
+++ src/pages/CarrosList/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from './style';
+import Header from "../../components/Header";
+
+export default function CarrosList(){
+    return(
+        <Container>
+            <Header titulo="Carros" />
+        </Container>
+    );
+}
