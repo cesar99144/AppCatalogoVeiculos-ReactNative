58c2a68c567ccefc7d09cdb4128df60e9e2d7f68
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-21 21:41:06 -0300

    Update App.js

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Routes from './src/routes';
+import LoadRoutes from './src/routes/loadRoutes';
 import { StatusBar } from 'expo-status-bar';
 
 export default function App(){
     return(
       <NavigationContainer>
           <StatusBar hidden={true} />
-          <Routes />
+          <LoadRoutes />
       </NavigationContainer>
     );
 }
