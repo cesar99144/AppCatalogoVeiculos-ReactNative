311b26ab11b14158ab1a30bda3901522b25e2f4a
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-21 21:41:23 -0300

    Update index.js

--- src/pages/CarrosList/index.js
+++ src/pages/CarrosList/index.js
@@ -1,11 +1,67 @@
-import React from "react";
-import { Container } from './style';
+import React, {useState, useEffect} from "react";
+import {ScrollView, RefreshControl} from 'react-native';
 import Header from "../../components/Header";
+import {} from "react-native-gesture-handler";
+import { 
+    Container,
+    CardVeiculos,
+    SearchContainer,
+    SearchButton,
+    Input,
+
+} from './style';
+
+import {Feather} from '@expo/vector-icons';
+import ListVeiculos from "../../components/ListVeiculos";
+import Api from '../../services/api';
 
 export default function CarrosList(){
+
+    const [refreshing, setRefreshing] = useState(false);
+    const [carros, setCarros] = useState([]);
+
+    async function getCarros(){
+        const response = await Api.get('/carros');
+        setCarros(response.data);
+    }
+
+    useEffect( () => {
+
+        getCarros();
+
+    }, []);
+
+    const onRefresh = () =>{
+        setRefreshing(false);
+        getCarros();   
+    }
+
+
     return(
         <Container>
             <Header titulo="Carros" />
+
+            <ScrollView 
+                showsVerticalScrollIndicator={false}
+                refreshControl={
+                    <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }>
+                <SearchContainer>
+                    <Input
+                    placeholder="Veículos"
+                    placeholderTextColor="#FFF"
+                    />
+                    <SearchButton>
+                        <Feather name="search" size={30} color="#FFF" />
+                    </SearchButton>
+                </SearchContainer>
+                <CardVeiculos
+                    data={carros}
+                    keyExtractor={ item => String(item.idVeiculo)}
+                    renderItem={ ( { item }) => <ListVeiculos data={item} />}
+                />
+            </ScrollView>
+            
         </Container>
     );
 }
