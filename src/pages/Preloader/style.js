09b3dfdbf2f5da7df8c985d59a6bc85c4472d483
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-21 21:41:36 -0300

    Create style.js

---
+++ src/pages/Preloader/style.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    background: #0F2741;
+    flex: 1;
+`;
+
+export const AreaCirculo = styled.View`
+    justify-content: space-space-around;
+`;
+
+export const Circulo = styled.View`
+ 
+   background: #ECBC1F;
+   width: 100px;
+	height: 100px;
+    border-top-left-radius: 50px;
+    border-bottom-left-radius: 50px;
+	overflow: hidden;
+	margin: 15px;
+    
+`;
