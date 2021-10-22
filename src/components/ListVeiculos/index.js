d8b3aecc3c8cadbe43f13db7d11503444bc288a9
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-21 21:41:16 -0300

    Create index.js

---
+++ src/components/ListVeiculos/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {ScrollView} from 'react-native';
+import {} from "react-native-gesture-handler";
+import{
+    Container,
+    Card,
+    ImagemCapa,
+    TextTitulo,
+    ContainerDescricao,
+    BotaoVizualizar,
+    TextoBotao,
+    TextPreco,
+} from './style';
+
+export default function ListVeiculos({data}){
+
+    
+    return(
+        <Container>
+            <Card activeOpacity={0.7}>
+                <ImagemCapa source={{uri: `http://192.168.1.9:4000/imagensVeiculos/${data.primeiraImagem}`}} />
+                <ContainerDescricao>
+                    <TextTitulo>{data.tituloAnuncio}</TextTitulo>
+                    <TextPreco>
+                        R$ {data.preco}
+                    </TextPreco>
+                    <BotaoVizualizar onPress={ () => alert('hello') }>
+                        <TextoBotao>Ver mais</TextoBotao>
+                    </BotaoVizualizar>
+                </ContainerDescricao>
+            </Card>
+        </Container>
+    );
+}
