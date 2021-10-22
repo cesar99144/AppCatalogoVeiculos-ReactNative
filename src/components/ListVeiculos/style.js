f5cc65158df04f76c5be2b4c004c5acf5e877276
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-21 21:41:18 -0300

    Create style.js

---
+++ src/components/ListVeiculos/style.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const Container = styled.View`
+    align-items: center;
+`;
+
+export const Card = styled.TouchableOpacity`
+    background-color: #FFFFFF;
+    margin-top: 20px;
+    border-radius: 20px;
+    padding: 10px;
+    flex-direction: row;
+    width: 98%;
+`;
+
+export const ImagemCapa = styled.Image`
+    width: 88px;
+    height: 88px;
+    border-radius: 20px;
+    
+`;
+
+export const ContainerDescricao = styled.View`
+    margin-left: 20px;
+    width: 200px;
+    justify-content: space-between;
+`;
+
+export const TextTitulo = styled.Text`
+    font-size: 17px;
+    font-weight: bold;
+`;
+
+export const TextPreco = styled.Text`
+    font-size: 16px;
+`;
+
+export const BotaoVizualizar = styled.TouchableOpacity`
+    width: 85px;
+    height: 26px;
+    border: 1px solid #4EADBE;
+    border-radius: 10px;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const TextoBotao = styled.Text`
+    font-size: 13px;
+    color: #268596;
+    
+`;
