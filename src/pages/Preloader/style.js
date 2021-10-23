8a98d03db4cf59faabbaa2fcdc7a9aafbdf4edfd
César Ferreira <cesarferreiraa67@gmail.com>
2021-10-23 17:00:06 -0300

    Update style.js

--- src/pages/Preloader/style.js
+++ src/pages/Preloader/style.js
@@ -6,17 +6,11 @@ export const Container = styled.View`
 `;
 
 export const AreaCirculo = styled.View`
-    justify-content: space-space-around;
+    
 `;
 
 export const Circulo = styled.View`
  
-   background: #ECBC1F;
-   width: 100px;
-	height: 100px;
-    border-top-left-radius: 50px;
-    border-bottom-left-radius: 50px;
-	overflow: hidden;
-	margin: 15px;
+   
     
 `;
